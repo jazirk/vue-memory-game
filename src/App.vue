0a78b6fa57adbbbf03d8e1d2958a5574c276a9e7
Jasir K <jasir.k@factset.com>
2022-08-20 19:41:20 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header><em>Memory Game</em></header>
+  <div class="container">
+    <Button v-if="!start" class="btn-start" @click="start = true">Start</Button>
+    <Block
+      v-else
+      v-for="(number, index) in randomNumber"
+      :key="index"
+      :value="number"
+      :disabled="disabled[index]"
+      :disableClick="disableClick"
+      @onClick="onBlockClick"
+    />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Block from "./components/Block.vue";
+import { computed, ref, watch } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const start = ref(false);
+
+const num1 = ref(null);
+const num2 = ref(null);
+
+const disableClick = ref(false);
+
+const disabled = ref(new Array(18).fill(false));
+
+const randomNumber = computed(() => {
+  const nums1 = new Set();
+  while (nums1.size !== 18) {
+    nums1.add(Math.floor(Math.random() * 18) + 1);
   }
-}
+
+  const nums2 = new Set();
+  while (nums2.size !== 18) {
+    nums2.add(Math.floor(Math.random() * 18) + 1);
+  }
+
+  console.log(nums1, nums2);
+  return [...nums1, ...nums2];
+});
+
+const onBlockClick = (val) => {
+  if (!num1.value && !num2.value) {
+    num1.value = val;
+  } else if (!num2.value) {
+    num2.value = val;
+    disableClick.value = true;
+    let timer = setTimeout(() => {
+      console.log(num1.value);
+
+      if (num1.value === num2.value) {
+        randomNumber.value.forEach((num, index) => {
+          if (num === num1.value || num === num2.value) {
+            disabled.value[index] = true;
+          }
+        });
+      }
+      num1.value = null;
+      num2.value = null;
+      disableClick.value = false;
+      clearTimeout(timer);
+    }, 3000);
+  } else {
+    //
+  }
+};
+
+watch("disabled", (val) => {
+  if (val.every((v) => v)) {
+    start.value = false;
+  }
+});
 </script>
 
 <style>
@@ -21,6 +82,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  width: 80%;
+  height: 80%;
+  place-items: center;
+  gap: 5px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+header {
+  font-size: 2em;
+}
+.btn-start {
+  background-color: #37b386;
+  color: white;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  position: absolute;
+  height: 100px;
+  font-size: 2em;
+  cursor: pointer;
+  width: 100px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,83 @@
+<template>
+  <Transition>
+    <div
+      :class="[{ block: true, disable: disableClick, visbility: disabled }]"
+      @click="onBlockClick"
+    >
+      {{ checkedNumber || "" }}
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0,
+  },
+  disabled: {
+    type: Boolean,
+  },
+  disableClick: {
+    type: Boolean,
+  },
+});
+// eslint-disable-next-line no-undef
+const emits = defineEmits(["onClick"]);
+
+console.log(props.disableClick);
+const checkedNumber = ref(0);
+
+const onBlockClick = () => {
+  checkedNumber.value = props.msg;
+  emits("onClick", checkedNumber.value, props.index);
+};
+
+watch(
+  () => props.disableClick,
+  (val) => {
+    console.log(val);
+    if (!val) {
+      checkedNumber.value = 0;
+    }
+  }
+);
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.block {
+  font-size: 1.5rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100px;
+  background-color: #b3ad37;
+  color: white;
+  cursor: pointer;
+}
+
+.disable {
+  cursor: not-allowed;
+  pointer-events: none;
+}
+
+.visbility {
+  visibility: hidden;
+}
+
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
